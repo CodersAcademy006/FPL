a1f104c9877e4a0471c4476a9c62ec7efa43242b
Srijan Upadhyay <104912634+CodersAcademy006@users.noreply.github.com>
2024-10-05 22:36:25 +0530

    Completed Question No. 4

---
+++ Arrays, Pointers and Functions/4.c
@@ -0,0 +1,25 @@
+// Write C programs that use both recursive and non-recursive functions.
+
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+int nonRecursiveFactorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+        result *= i;
+    return result;
+}
+
+int main() {
+    int n;
+    printf("Enter a number to find its factorial: ");
+    scanf("%d", &n);
+
+    printf("Recursive Factorial: %d\n", factorial(n));
+    printf("Non-Recursive Factorial: %d\n", nonRecursiveFactorial(n));
+    return 0;
+}
